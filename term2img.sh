9b1660768216938ad25cfeca443db5c92f3acfe3
Roshan-R <roshanr2001@gmail.com>
2020-08-21 19:28:19 +0530

    added help message

--- term2img.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-script -c $1 -O tmp
-cat tmp | sed -e "s/[^M^]^A^H^[^G]//g" | sed "1d" | sed "$ d" | convert label:@- output.png
-rm tmp
---
+++ termshot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+Help()
+{
+    echo "term2img - Record application output and convert to image"
+    echo
+    echo "Syntax: term2img executable outputimage"
+    echo
+    echo "executable  : path to the executable file"
+    echo "outputimage : Name of the ouput image file"
+}
+
+if [[ $# == 2 ]];then
+    echo $#
+    Help
+else
+    script -c "$1" -O tmp
+    cat tmp | sed -e "s/[^M^]^A^H^[^G]//g" | sed "1d" | sed "$ d" | convert label:@- "$2"
+    rm tmp
+fi
